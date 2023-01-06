7ff093a33af5ae9d8587b8297c926b011d122033
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-06 20:42:12 -0300

    add more products

--- backend/products.js
+++ backend/products.js
@@ -1,10 +1,10 @@
 const products=[
     {
         id:1,
-        name: "Shape element",
+        name: "Shape Santa Cruz",
         desc: "maple, 8.0 ",
         price: "350.00",
-        image: "https://res.cloudinary.com/debntid8t/image/upload/v1672323518/NorthWestSkateShop/shape-element_vgwjdk.jpg"
+        image: "https://res.cloudinary.com/debntid8t/image/upload/v1673040989/NorthWestSkateShop/AG.375-Melhores-Shapes-de-Skate-de-2022-_-Qual-o-Melhor_xtxdfh.jpg"
     },
 
     {
@@ -21,7 +21,31 @@ const products=[
         desc: "branca, GG ",
         price: "95.00",
         image: "https://res.cloudinary.com/debntid8t/image/upload/v1672323519/NorthWestSkateShop/camisa-flip_c10waz.jpg"
-    }
+    },
+
+    {
+        id:4,
+        name: "Lixa MOB Grip",
+        desc: "Lixa MOB Grip Black 9 X 33",
+        price: "79.90",
+        image:"https://res.cloudinary.com/debntid8t/image/upload/v1673041834/lixa_vyezjd.jpg",
+    },
+
+    {
+        id:5,
+        name: "Roda Moska",
+        desc: "Rock 52mm 53D Branca",
+        price: "179.90",
+        image: "https://res.cloudinary.com/debntid8t/image/upload/v1673041834/rodinha_t98u8f.jpg", 
+    },
+
+    {
+        id:6,
+        name: "Truck Intruder",
+        desc: "149mm Pro Noble Séries",
+        price: "224.90",
+        image: "https://res.cloudinary.com/debntid8t/image/upload/v1673041834/truck_f4qz6s.jpg", 
+    },
 
 
 
