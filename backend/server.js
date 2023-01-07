a84598739239798b5cf879b6d42f06a1c821eb41
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-07 02:18:25 -0300

    eLaVamosNos

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://ecomercer-main-front.vercel.app");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
