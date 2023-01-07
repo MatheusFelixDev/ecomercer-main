700f8229178c5909f5f7009aab4a82beb088ac1a
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-07 00:48:15 -0300

    test

--- frontend/src/features/productsApi.js
+++ frontend/src/features/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
     reducerPath: "productsApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000"}),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://ecomercer-main.vercel.app/"}),
     endpoints: (builder) =>({
         getAllProducts: builder.query({
             query:() => "products",
