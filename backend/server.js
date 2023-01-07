114c82a0ab8a72d2a70bd09325d2f4d366110c43
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-07 02:26:17 -0300

    fffff

--- backend/server.js
+++ backend/server.js
@@ -11,13 +11,11 @@ const app = express();
 require("dotenv").config()
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://ecomercer-main-front.vercel.app/'
+}));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://ecomercer-main-front.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 app.use("/register", register);
 app.use("/login", login);
