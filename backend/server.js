fd0cd5262ba01495f1450130f2f7d2dcd752e71f
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-09 18:27:19 -0300

    333

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 
 app.use((req, res, next) =>{
   res.header("Access-Control-Allow-Origin", "https://ecomercer-main-front.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header('Access-Control-Allow-Methods', '*');
   app.use(cors());
   next();
 });
