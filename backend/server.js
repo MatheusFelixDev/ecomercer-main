368bae546f75beb72285fb2bb81b07cb1f6628d6
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-10 13:14:20 -0300

    sdw

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,14 @@ app.use(express.json());
 //  console.log("Adding Access-Control-Allow-Origin header");
 //  res.setHeader("Access-Control-Allow-Origin", "*");
 //});
-
-
-
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
 
 app.use("/api/register", register);
 app.use("/api/login", login);
@@ -38,11 +43,3 @@ const port = process.env.PORT || 5000
 
 app.listen(port, console.log(`Server running on port ${port}`));
 
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connection established..."))
-  .catch((error) => console.error("MongoDB connection failed:", error.message));
