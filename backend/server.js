087ab9b6f4d029c8ddfdfa773e03e0118abbccaa
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-10 13:26:25 -0300

    ssad

--- backend/server.js
+++ backend/server.js
@@ -30,14 +30,7 @@ app.get("/products", (req, res) => {
     res.send(products); 
 });
 
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connection established..."))
-  .catch((error) => console.error("MongoDB connection failed:", error.message));
+
 
 
 
@@ -46,5 +39,14 @@ const port = process.env.PORT || 5000
 
 
 app.listen(port, console.log(`Server running on port ${port}`));
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
 
 
