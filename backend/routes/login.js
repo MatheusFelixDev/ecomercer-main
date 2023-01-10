1b049b8c55ee1d97b4d54b738962d1a456bff492
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-10 11:03:10 -0300

    mais um dia

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 
 router.post("/", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://ecomercer-main-front.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://ecomercer-main-front.vercel.app/api/login");
 
 
   const schema = Joi.object({
