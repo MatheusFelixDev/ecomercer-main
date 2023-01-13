8e62a137d3d075563ba60f9d48ee3b9db4cc111f
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-13 19:18:10 -0300

    005

--- frontend/src/features/api.js
+++ frontend/src/features/api.js
@@ -1,1 +1,11 @@
-export const url = "https://ecomercer-main.vercel.app/api"
+export const url = "https://ecomercer-main.vercel.app/api";
+
+export const setHeaders = () => {
+  const headers = {
+    headers: {
+      "x-auth-token": localStorage.getItem("token"),
+    },
+  };
+
+  return headers;
+};
