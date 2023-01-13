63dbfecfe7a0a683034aa386335c90ae7bdc70a0
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-13 02:24:14 -0300

    001

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import Home from './components/Home';
 import NotFound from './components/NotFound';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
-import CheckoutSuccess from './components/CheckoutSuccess';
+import CheckoutSuccess from './components/checkoutSuccess';
 import Dashboard from './components/admin/Dashboard';
 import Products from './components/admin/Products';
 import Summary from './components/admin/Summary';
