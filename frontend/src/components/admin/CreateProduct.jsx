e952f105a133dcb3122cdea2b00710c12bc374d3
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-13 18:52:44 -0300

    002

--- frontend/src/components/admin/CreateProduct.jsx
+++ frontend/src/components/admin/CreateProduct.jsx
@@ -1,145 +1,148 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { productsCreate } from "../../features/productsSlice";
 import { PrimaryButton } from "./CommonStyled";
+import { productsCreate } from "../../slices/productsSlice";
 
 const CreateProduct = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { createStatus } = useSelector((state) => state.products);
 
-    const [productImg, setProductImg] = useState("");
-    const [name, setName] = useState("");
-    const [brand, setBrand] = useState("");
-    const [price, setPrice] = useState("");
-    const [desc, setDesc] = useState("");
+  const [productImg, setProductImg] = useState("");
+  const [brand, setBrand] = useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [desc, setDesc] = useState("");
 
-    console.log(productImg);
+  const handleProductImageUpload = (e) => {
+    const file = e.target.files[0];
 
-    const handleProductImageUpload = (e) => {
-        const file = e.target.files[0];
+    TransformFileData(file);
+  };
 
-        TransformFile(file)
-    };
+  const TransformFileData = (file) => {
+    const reader = new FileReader();
 
-    const TransformFile = (file) =>{
-        const reader = new FileReader()
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        setProductImg(reader.result);
+      };
+    } else {
+      setProductImg("");
+    }
+  };
 
-        if (file) {
-            reader.readAsDataURL(file)
-            reader.onloadend = () => {
-                setProductImg(reader.result);
-            };
-        }else{
-            setProductImg("");
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        dispatch(
-            productsCreate({
-                name,
-                brand,
-                price,
-                desc,
-                image: productImg
-            })
-        );
-    };
-
-    return (
-        <StyledCreateProduct>
-            <StyledForm onSubmit={handleSubmit}>
-                <h3>Crie um Produto</h3>
-                <input 
-                    type="file" 
-                    accept="image/" 
-                    onChange={handleProductImageUpload}
-                    required
-                />
-                <select onChange={(e) => setBrand(e.target.value)} required>
-                    <option value="">Categoria</option>
-                    <option value="roupa">roupa</option>
-                    <option value="skate">skate</option>
-                    <option value="tenis">tenis</option>
-                    <option value="outro">outro</option>
-                </select>
-                <input
-                    type="text"
-                    required
-                    placeholder="Name"
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <input
-                    type="text"
-                    required
-                    placeholder="Price"
-                    onChange={(e) => setPrice(e.target.value)}
-                />
-                <input
-                    type="text"
-                    required
-                    placeholder="Descrição"
-                    onChange={(e) => setDesc(e.target.value)}
-                />
-                <PrimaryButton type="submit">Comfirma</PrimaryButton>
-            </StyledForm>
-            <ImagePreview>
-                {productImg ? (
-                    <>
-                        <img src={productImg} alt="Product image" />
-                    </> 
-                ):( 
-                    <p>A visualização da imagem aparecerá aqui!</p>
-                )}
-            </ImagePreview>
-        </StyledCreateProduct>
+    dispatch(
+      productsCreate({
+        name,
+        brand,
+        price,
+        desc,
+        image: productImg,
+      })
     );
+  };
+
+  return (
+    <StyledCreateProduct>
+      <StyledForm onSubmit={handleSubmit}>
+        <h3>Create a Product</h3>
+        <input
+          id="imgUpload"
+          accept="image/*"
+          type="file"
+          onChange={handleProductImageUpload}
+          required
+        />
+        <select onChange={(e) => setBrand(e.target.value)} required>
+            <option value="">Categoria</option>
+            <option value="roupa">roupa</option>
+            <option value="skate">skate</option>
+            <option value="tenis">tenis</option>
+            <option value="outro">outro</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Short Description"
+          onChange={(e) => setDesc(e.target.value)}
+          required
+        />
+
+        <PrimaryButton type="submit">
+          {createStatus === "pending" ? "Submitting" : "Submit"}
+        </PrimaryButton>
+      </StyledForm>
+      <ImagePreview>
+        {productImg ? (
+          <>
+            <img src={productImg} alt="error!" />
+          </>
+        ) : (
+          <p>Product image upload preview will appear here!</p>
+        )}
+      </ImagePreview>
+    </StyledCreateProduct>
+  );
 };
 
 export default CreateProduct;
 
-const StyledCreateProduct = styled.div`
-    display: flex;
-    justify-content: space-between;
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  margin-top: 2rem;
+  select,
+  input {
+    padding: 7px;
+    min-height: 30px;
+    outline: none;
+    border-radius: 5px;
+    border: 1px solid rgb(182, 182, 182);
+    margin: 0.3rem 0;
+    &:focus {
+      border: 2px solid rgb(0, 208, 255);
+    }
+  }
+  select {
+    color: rgb(95, 95, 95);
+  }
 `;
 
-const StyledForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    max-width: 300px;
-    margin-top: 2rem;
-    select,
-    input{
-        padding: 7px;
-        min-height: 30px;
-        outline: none;
-        border-radius: 5px;
-        border: 1px solid rgb(182, 182, 182);
-        margin: 0.3rem 0;
-        &:focus{
-            border: 2px solid rgb(0, 208, 255);
-        }
-    }
-    select{
-        color: rgb(95, 95, 95)
-    }
+const StyledCreateProduct = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const ImagePreview = styled.div`
-    margin: 2rem 0 2rem 2rem;
-    padding: 2rem;
-    border: 1px solid rgb(183, 183, 183);
-    max-width: 300px;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 2rem;
-    color: rgb(78, 78, 78);
-    img{
-        max-width: 100%;
-    }
+  margin: 2rem 0 2rem 2rem;
+  padding: 2rem;
+  border: 1px solid rgb(183, 183, 183);
+  max-width: 300px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  color: rgb(78, 78, 78);
+  img {
+    max-width: 100%;
+  }
 `;
-
