54f35d66cf41e44997e3c9037952bf72a2aa01df
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-13 19:10:44 -0300

    005

--- frontend/src/features/productsSlice.js
+++ frontend/src/features/productsSlice.js
@@ -1,72 +1,72 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { url } from "./api";
+import { url, setHeaders } from "./api";
 import { toast } from "react-toastify";
 
 const initialState = {
-    items: [],
-    status: null,
-    createStatus: null,
-    
+  items: [],
+  status: null,
+  createStatus: null,
 };
 
 export const productsFetch = createAsyncThunk(
-    "products/productsFetch",
-    async() => {
-        try{
-            const response = await axios.get(
-                "https://ecomercer-main.vercel.app"
-            );
-            return response.data;
-        }catch (error){
-            console.log(error)
-        }
+  "products/productsFetch",
+  async () => {
+    try {
+      const response = await axios.get(`${url}/products`);
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
     }
+  }
 );
 
 export const productsCreate = createAsyncThunk(
-    "products/productsCreate",
-    async(values) => {
-       try {
-            const response = await axios.post(
-                `${url}/products`, values
-            );
-            return response.data;
-       }catch (error){
-        console.log(error);
-        toast.error(error.response?.data);
-       }
+  "products/productsCreate",
+  async (values) => {
+    try {
+      const response = await axios.post(
+        `${url}/products`,
+        values,
+        setHeaders()
+      );
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response?.data);
     }
+  }
 );
 
 const productsSlice = createSlice({
-    name: "products",
-    initialState,
-    reducers:{},
-    extraReducers: {
-        [productsFetch.pending]: (state, action) => { 
-            state.status = "pending";
-        },
-        [productsFetch.fulfilled]: (state, action) => {
-            state.items = action.payload;
-            state.status = "success";
-        },
-        [productsFetch.rejected]: (state, action) =>{
-            state.status = "rejected";
-            
-        },
-
-        [productsCreate.pending]: (state, action) => { 
-            state.CreateStatus = "pending";
-        },
-        [productsCreate.fulfilled]: (state, action) => {
-            state.items.push(action.payload);
-            state.CreateStatus = "success";
-        },
-        [productsCreate.rejected]: (state, action) =>{
-            state.CreateStatus = "rejected";  
-        },
-    }
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [productsFetch.pending]: (state, action) => {
+      state.status = "pending";
+    },
+    [productsFetch.fulfilled]: (state, action) => {
+      state.items = action.payload;
+      state.status = "success";
+    },
+    [productsFetch.rejected]: (state, action) => {
+      state.status = "rejected";
+    },
+    [productsCreate.pending]: (state, action) => {
+      state.createStatus = "pending";
+    },
+    [productsCreate.fulfilled]: (state, action) => {
+      state.items.push(action.payload);
+      state.createStatus = "success";
+      toast.success("Product Created!");
+    },
+    [productsCreate.rejected]: (state, action) => {
+      state.createStatus = "rejected";
+    },
+  },
 });
 
 export default productsSlice.reducer;
