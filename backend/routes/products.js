b56483e7e4d63fd2bbbf43bfb05b3534aa29f48f
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-13 23:21:51 -0300

    006

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,46 +1,97 @@
-const express = require("express");
-const cloudinary = require ("../utils/cloudinary");
+const { Product } = require("../models/Product");
+const { auth, isUser, isAdmin } = require("../middleware/auth");
+const cloudinary = require("../utils/cloudinary");
 
-const  router = express.Router();
+const router = require("express").Router();
 
-//create product
+//CREATE
 
-router.post ("/", async (req, res) => {
-    const {name, brand, desc, price, image} = req.body;
+router.post("/", isAdmin, async (req, res) => {
+  const { name, brand, desc, price, image } = req.body;
 
-    try {
-        if(image){
-            const uploadRes = await cloudinary.uploader.upload(image, {
-                upload_preset: "northwesth-skateshop"
-            })
+  try {
+    if (image) {
+      const uploadedResponse = await cloudinary.uploader.upload(image, {
+        upload_preset: "online-shop",
+      });
 
-            if(uploadRes) {
-                const product = new Product({
-                    name,
-                    brand,
-                    desc,
-                    price,
-                    image: uploadRes,
-                });
+      if (uploadedResponse) {
+        const product = new Product({
+          name,
+          brand,
+          desc,
+          price,
+          image: uploadedResponse,
+        });
 
-                const savedProduct = await product.save();
-
-                res.status(200).send(savedProduct);
-            }
-        }
-    }catch(error){
-        console.log(error);
-        res.status(500).send(error);
+        const savedProduct = await product.save();
+        res.status(200).send(savedProduct);
+      }
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 });
 
-router.get("/", async(req, res) =>{
-    try{
-        const products = await Product.find()
-        res.status(200).send(products)
-    }catch(error){
-        res.status(500).send(error);
+//DELETE
+
+router.delete("/:id", isAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).send("Product has been deleted...");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//GET ALL PRODUCTS
+
+router.get("/", async (req, res) => {
+  const qbrand = req.query.brand;
+  try {
+    let products;
+
+    if (qbrand) {
+      products = await Product.find({
+        brand: qbrand,
+      });
+    } else {
+      products = await Product.find();
     }
-})
+
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//GET PRODUCT
+
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//UPDATE
+
+router.put("/:id", isAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).send(updatedProduct);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
