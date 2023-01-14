5068ed05e2d35498a2ee7aa815fa359706a330d4
MatheusFelixDev <MatheusFelix.Dev@gmail.com>
2023-01-13 23:43:03 -0300

    008

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const register = require("./routes/register");
+const orders = require("./routes/orders");
 const login = require("./routes/login");
 const stripe = require("./routes/stripe");
 const productsRoute = require("./routes/products");
@@ -22,6 +23,7 @@ app.use(express.json());
 
 app.use("/api/register", register);
 app.use("/api/login", login);
+app.use("/api/orders", orders);
 app.use("/api/stripe", stripe);
 app.use("api/products", productsRoute);
 
